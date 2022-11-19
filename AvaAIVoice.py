df637f349bb5c4999b1fb9adde41a2d33dbbc47d
Josode <soderberg5@yahoo.com>
2022-11-19 14:52:53 -0700

    Update AvaAIVoice.py
    cleaned some code up.

--- AvaAIVoice.py
+++ AvaAIVoice.py
@@ -8,16 +8,17 @@ openai.api_key = API_KEY
 
 engine = pyttsx3.init()
 
-r= sr.Recognizer()
-mic = sr.Microphone(device_index=2)
+r = sr.Recognizer()
+mic = sr.Microphone(device_index=1)
 
 
 conversation = ""
-user_name = "Josode"
+user_name = "You"
+bot_name = "Jarvis"
 
 while True:
     with mic as source:
-        print("\nlistening... speak clearly into mic.")
+        print("\nlistening...")
         r.adjust_for_ambient_noise(source, duration=0.2)
         audio = r.listen(source)
     print("no longer listening.\n")
@@ -27,14 +28,14 @@ while True:
     except:
         continue
 
-    prompt = user_name + ": " + user_input + "\n Ava:"
+    prompt = user_name + ": " + user_input + "\n" + bot_name+ ": "
 
-    conversation += prompt
+    conversation += prompt  # allows for context
 
+    # fetch response from open AI api
     response = openai.Completion.create(engine='text-davinci-001', prompt=conversation, max_tokens=100)
     response_str = response["choices"][0]["text"].replace("\n", "")
-    response_str = response_str.split(user_name + ": ", 1)[0].split("Ava: ", 1)[0]
-
+    response_str = response_str.split(user_name + ": ", 1)[0].split(bot_name + ": ", 1)[0]
 
     conversation += response_str + "\n"
     print(response_str)
